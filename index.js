18c5aa75665e4735517f8fb6487cc6a0048d48f5
Gatfanoff <Gatfanovm@mail.ru>
2023-10-11 17:52:52 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const character = {
+  name: "Лучник",
+  type: "Bowman",
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: "Двойной выстрел",
+      icon: "http://...",
+      description: "Двойной выстрел наносит двойной урон",
+    },
+    {
+      id: 9,
+      name: "Нокаутирующий удар",
+      icon: "http://...",
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+function extractSpecialAttacks({ special }) {
+  return special.map((el) => ({
+    id: el.id,
+    name: el.name,
+    description: el.description || "Описание недоступно",
+    icon: el.icon,
+  }));
+}
